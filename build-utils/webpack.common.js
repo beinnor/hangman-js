37a1774beb5ba0b264de1539791d803ca917c1d7
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-11-27 22:52:17 +0100

    fix for gh-pages

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, '../', 'dist'),
-    publicPath: '/',
+    publicPath: '/hangman-js/',
     filename: 'bundle.js',
   },
   devServer: {
